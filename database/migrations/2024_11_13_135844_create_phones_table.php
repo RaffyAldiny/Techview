6c7d1e87890c4fdc667b69aa477c7b861bdf9f1c
Yugi <84443157+RaffyAldiny@users.noreply.github.com>
2024-11-13 22:07:34 +0800

    filament set up

---
+++ database/migrations/2024_11_13_135844_create_phones_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('phones', function (Blueprint $table) {
+            $table->id('phone_id');
+            $table->string('name');
+            $table->string('brand', 255);
+            $table->date('release_date');
+            $table->foreignId('phonespecification_id')->constrained('phone_specifications');
+            $table->text('description')->nullable();
+            $table->boolean('is_featured')->default(false);
+            $table->string('image_path')->nullable();
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('phones');
+    }
+};
